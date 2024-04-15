093981af1ceb9c545e0fc84773ee582340b57efd
mmjustme <infooneuser@gmail.com>
2024-04-15 21:24:16 +0300

    Create basic react-router-dom and project tree structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import { SigninForm, SignupForm } from "./_auth/forms";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* publick routes (sign in, sign up) */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* publick routes (can see only sign in) */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   );
 };
 
