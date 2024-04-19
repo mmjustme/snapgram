80eb60a00ae7461708f96496f3fb68b5d2aaea48
D <166647954+mmjustme@users.noreply.github.com>
2024-04-19 12:02:15 +0000

    SignupForm: 1. Add form, input,button components from shadcn. 2. Updated validation. 3. Create input structure. 4. Add loader to the signup btn.

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,14 @@
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+      <img
+        src="https://raw.githubusercontent.com/adrianhajdin/social_media_app/9fa032dd198c92aa498db82d248c0d93f28ebf5c/public/assets/icons/loader.svg"
+        alt="loader"
+        width={24}
+        height={24}
+      />
+    </div>
+  );
+};
+
+export default Loader;
