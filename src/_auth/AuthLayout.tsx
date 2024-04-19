673db476ab6e52f88a7023e82de9c1f75e9e46be
D <166647954+mmjustme@users.noreply.github.com>
2024-04-19 10:45:59 +0000

    Create basic layout for sign-in and sign-up pages in the AuthLayout.

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,26 @@
+import { Navigate, Outlet } from "react-router-dom";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to={"/"} />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img
+            // TODO change url to img
+            src="https://raw.githubusercontent.com/adrianhajdin/social_media_app/9fa032dd198c92aa498db82d248c0d93f28ebf5c/public/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
