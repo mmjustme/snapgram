033f898fdbc932969880ede3744648299957e2ca
D <166647954+mmjustme@users.noreply.github.com>
2024-04-25 10:28:09 +0000

    1. Created SigninForm based on SignupForm. 2. Created SigninValidation. 3. Small update authContext for correct work Loader in SigninForm.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -5,89 +5,90 @@ import { useNavigate } from "react-router-dom";
 
 // тут визначаємо як виглядає пустий юзер
 export const INITIAL_USER = {
-    id: "",
-    name: "",
-    username: "",
-    email: "",
-    imageUrl: "",
-    bio: "",
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  imageUrl: "",
+  bio: "",
 };
 
 // тут визначаємо початковий стан Auth щоб постійно розуміти стан юзера
 const INITIAL_STATE = {
-    user: INITIAL_USER,
-    isLoading: false,
-    isAuthenticated: false,
-    setUser: () => { },
-    setIsAuthenticated: () => { },
-    checkAuthUser: async () => false as boolean,
+  user: INITIAL_USER,
+  isLoading: false,
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean,
 };
 
 //  "<IUser>" таким чином ми даємо точно знати typescript як виглядаюзер
 const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const [user, setUser] = useState<IUser>(INITIAL_USER);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    //   we need call after reload page so use useEffect
-    const checkAuthUser = async () => {
-        try {
-            // fn directly from appwrite
-            const currentAccount = await getCurrentUser();
+  //   we need call after reload page so use useEffect
+  const checkAuthUser = async () => {
+    setIsLoading(true);
+    try {
+      // fn directly from appwrite
+      const currentAccount = await getCurrentUser();
 
-            // if currentAccount exist => set our user
-            if (currentAccount) {
-                setUser({
-                    id: currentAccount.$id,
-                    name: currentAccount.name,
-                    username: currentAccount.username,
-                    email: currentAccount.email,
-                    imageUrl: currentAccount.imageUrl,
-                    bio: currentAccount.bio,
-                });
+      // if currentAccount exist => set our user
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        });
 
-                setIsAuthenticated(true);
+        setIsAuthenticated(true);
 
-                return true;
-            }
+        return true;
+      }
 
-            // if currentAccount don't exist => return false
-            return false;
-        } catch (error) {
-            console.log(error);
-            // false = user not authenticated
-            return false;
-        } finally {
-            // mean we done with loading
-            setIsLoading(false);
-        }
-    };
+      // if currentAccount don't exist => return false
+      return false;
+    } catch (error) {
+      console.log(error);
+      // false = user not authenticated
+      return false;
+    } finally {
+      // mean we done with loading
+      setIsLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        // localStorage.getItem("cookieFallback") === null
-        if (
-            localStorage.getItem("cookieFallback") === "[]" 
-        ) {
-            navigate("/sign-in");
-        }
+  useEffect(() => {
+    if (
+      localStorage.getItem("cookieFallback") === "[]" ||
+      localStorage.getItem("cookieFallback") === null
+    ) {
+      navigate("/sign-in");
+    }
 
-        checkAuthUser();
-    }, []);
+    checkAuthUser();
+  }, []);
 
-    const value = {
-        user,
-        setUser,
-        isLoading,
-        isAuthenticated,
-        setIsAuthenticated,
-        checkAuthUser,
-    };
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated,
+    checkAuthUser,
+  };
 
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -8,3 +8,9 @@ export const SignupValidaton = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
 });
+
+export const SigninValidation = z.object({ 
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Password must be at least 8 characters" }),
+});
+
