abc55491271a41ee58657f745486c33b48d1d02f
D <166647954+mmjustme@users.noreply.github.com>
2024-04-26 10:29:02 +0000

    1. Leftsidebar complete. 2. Complete structure root routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import "./globals.css";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import { SigninForm, SignupForm } from "./_auth/forms";
 import RootLayout from "./_root/RootLayout";
@@ -19,6 +29,14 @@ const App = () => {
         {/* publick routes (can see only sign in) */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
